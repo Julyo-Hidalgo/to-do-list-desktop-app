5daa7aa564b9e13ed2034f9c089d61062b26eaf2
julyo-hidalgo <julyohidalgo0@gmail.com>
2023-11-08 22:12:06 -0300

    Inital commit

---
+++ Program.java
@@ -0,0 +1,8 @@
+import Views.HomeForm;
+
+public class Program {
+    public static void main(String[] args) {
+        HomeForm homeForm = new HomeForm();
+        homeForm.setVisible(true);
+    }
+}
---
+++ Views/HomeForm.java
@@ -0,0 +1,18 @@
+package Views;
+
+import javax.swing.*;
+
+public class HomeForm extends JFrame {
+    public HomeForm() {
+        configureFrame();
+        initializeComponents();
+    }
+
+    public void configureFrame() {
+
+    }
+
+    public void initializeComponents() {
+
+    }
+}
